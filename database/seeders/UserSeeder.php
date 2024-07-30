d1d3bfb7e8afb20f6a9e240b046e68720fa46b9a
Jacob Deepu <d2jdeepu@gmail.com>
2024-07-30 10:14:51 +0530

    Create Super Admin role & grant full access
       Assign 'Super Admin' to 'Super Admin' user.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
 
 class UserSeeder extends Seeder
 {
@@ -13,11 +14,15 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        $super_admin = User::factory()->create([
+        Role::create(['name' => 'Super Admin']);
+
+        $superAdmin = User::factory()->create([
             'name' => 'Super Admin',
             'email' => 'superadmin@admin.com',
             'password' => Hash::make('password'),
         ]);
+
+        $superAdmin->assignRole('Super Admin');
 
         $admin = User::factory()->create([
             'name' => 'Admin',
