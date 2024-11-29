68fbd477576b39329e7a9ad2f7a2ae5cac85a380
Jacob Deepu <d2jdeepu@gmail.com>
2024-11-29 16:28:00 +0530

    Create model and migration for FormSubSection

---
+++ database/migrations/2024_11_29_161223_create_form_sub_sections_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('form_sub_sections', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('form_section_id')
+                ->cascadeOnDelete()
+                ->constrained();
+            $table->string('name');
+            $table->string('icon');
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('form_sub_sections');
+    }
+};
