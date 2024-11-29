f4026c2d7ae4a4dcccea988e6b2c1a0de627adc3
Jacob Deepu <d2jdeepu@gmail.com>
2024-11-29 16:41:25 +0530

    Create model and migration for FormSubSection

---
+++ app/Models/FormField.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FormField extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'label',
+        'type',
+        'options',
+        'is_required',
+        'validation_rules'
+    ];
+}
