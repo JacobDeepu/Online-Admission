b9b2ee1a7a0ef6dfc2f42224558fa20cdf04b38c
Jacob Deepu <d2jdeepu@gmail.com>
2024-12-03 15:28:12 +0530

    Add support for phosphor-icons

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -8,8 +9,9 @@
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link href="https://fonts.googleapis.com" rel="preconnect">
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        <link type="text/css" href="https://unpkg.com/@phosphor-icons/web@2.1.1/src/bold/style.css" rel="stylesheet" />
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
@@ -17,6 +19,7 @@
         <!-- Styles -->
         @livewireStyles
     </head>
+
     <body>
         <div class="font-sans text-gray-900 antialiased">
             {{ $slot }}
@@ -24,4 +27,5 @@
 
         @livewireScripts
     </body>
+
 </html>
