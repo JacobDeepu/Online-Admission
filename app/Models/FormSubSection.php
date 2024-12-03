252e8d261b37da2cf548c71d2baa8530736f7c2c
Jacob Deepu <d2jdeepu@gmail.com>
2024-12-03 15:25:49 +0530

    Add relations to FormSubSection

--- app/Models/FormSubSection.php
+++ app/Models/FormSubSection.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class FormSubSection extends Model
 {
@@ -21,4 +23,20 @@ class FormSubSection extends Model
         'order',
         'description',
     ];
+
+    /**
+     * Get the section that owns the subsection.
+     */
+    public function section(): BelongsTo
+    {
+        return $this->belongsTo(FormSection::class);
+    }
+
+    /**
+     * Get all of the subsections's fields.
+     */
+    public function fields(): MorphMany
+    {
+        return $this->morphMany(FormField::class, 'formable');
+    }
 }
