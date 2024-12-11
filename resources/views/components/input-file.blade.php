cf9c3b2a8a321e131ca1b6488cbdba8026b456f1
Jacob Deepu <d2jdeepu@gmail.com>
2024-12-11 20:14:19 +0530

    Update select, input-float-label & input-file components
        Add an option to set required attribute with a prop.
    
        This will fix issues on setting conditional required attribute from views.

--- resources/views/components/input-file.blade.php
+++ resources/views/components/input-file.blade.php
@@ -1,11 +1,11 @@
-@props(['name', 'label'])
+@props(['name', 'label', 'isRequired' => false])
 <label class="@error($name) text-red-500 @else text-gray-900 @enderror mb-2 block text-sm font-medium" for="{{ $name }}">
     {{ $label }}
-    @if ($attributes->has('required'))
+    @if ($isRequired || !$attributes->has('required'))
         <span class="text-red-600">*</span>
     @endif
 </label>
-<input name="{{ $name }}" type="file" @error($name) {!! $attributes->merge([
+<input name="{{ $name }}" type="file" @if ($isRequired && !$attributes->has('required')) required @endif @error($name) {!! $attributes->merge([
     'class' => 'border-red-600 cursor-pointer rounded-lg border bg-gray-50 p-2.5 text-sm text-gray-900
 focus:outline-none',
 ]) !!}
