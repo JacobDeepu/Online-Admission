4286b1b3c0cddf402162b5e575ee474e89838497
Jacob Deepu <d2jdeepu@gmail.com>
2024-12-11 22:49:20 +0530

    Fix dynamic color class not generated issue
        Add the colors we use in safelist, there isn't any other solution
        for now to generate the class for dynamic colors.
    
    TODO: Explore other ways to achieve the same and switch to that implimentation.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,33 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
-import typography from '@tailwindcss/typography';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
+import typography from "@tailwindcss/typography";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './vendor/laravel/jetstream/**/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./vendor/laravel/jetstream/**/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
     ],
-
+    safelist: [
+        {
+            pattern:
+                /bg-(blue|indigo|teal)-(50|100|200|300|400|500|600|700|800|900|950)/,
+        },
+        {
+            pattern:
+                /text-(blue|indigo|teal)-(50|100|200|300|400|500|600|700|800|900|950)/,
+        },
+    ],
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Inter', ...defaultTheme.fontFamily.sans],
+                sans: ["Inter", ...defaultTheme.fontFamily.sans],
             },
             backgroundImage: {
-                'hero-bg': "url('/public/images/bg.png')",
-            }
+                "hero-bg": "url('/public/images/bg.png')",
+            },
         },
     },
 
